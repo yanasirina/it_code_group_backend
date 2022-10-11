6791eb6d76f55c2d96a5cff770e7f92f195fbb45
Yana Sirina <yanasirina0@yandex.ru>
2022-10-12 02:08:55 +0500

    Создала сериализаторы

---
+++ core/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    display = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+    def get_display(self, obj: Tag):
+        return f"{obj.id}: {obj.name}"
+
+
+class TagSearch(serializers.Serializer):
+    name = serializers.CharField(label="Название", required=True)
+
+    def validate_name(self, value):
+        if not Tag.objects.filter(name__icontains=value):
+            raise serializers.ValidationError(f'Нет меток, содержащих "{value}"')
+        return value
+
+    def validate(self, attrs):
+        return attrs
