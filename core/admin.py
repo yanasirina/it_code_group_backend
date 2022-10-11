d0f22dcdf9f591e2931a32286a23b3d060740ad9
Yana Sirina <yanasirina0@yandex.ru>
2022-10-12 01:20:00 +0500

    Зарегистрировала модели в панели администратора

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Tag)
+class Tag(admin.ModelAdmin):
+    list_display = ("name", )
+    search_fields = ("name", )
+
+
+@admin.register(models.Item)
+class Item(admin.ModelAdmin):
+    list_display = ("name", "done")
+    search_fields = ("name", )
