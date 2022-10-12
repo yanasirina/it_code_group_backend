7e6f30f7d71bf62a2e0cdc5cbd5d5b3c3e46bb49
Yana Sirina <yanasirina0@yandex.ru>
2022-10-13 00:19:28 +0500

    Разрешила аутентификацию с помощью сессий и токенов

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
-from . import serializers, models
+from . import serializers
+from . import models
 
 
 class TagViewSet(ReadOnlyModelViewSet):
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
     queryset = models.Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
