a1e529b5dd980d91e12cd61cd73b00c9e1ad701c
Yana Sirina <yanasirina0@yandex.ru>
2022-10-18 15:01:24 +0500

    Прописала пути для регистрации и авторизации пользователя

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 import core.views
 
-urlpatterns = []
+
+urlpatterns = [
+    path('user/register/', core.views.RegisterUser.as_view(), name='user_register'),
+    path('user/login/', core.views.LoginUser.as_view(),  name='user_login'),
+]
+
 
 router = DefaultRouter()
 router.register('tags', core.views.TagViewSet, basename='tag')
