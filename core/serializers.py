b6eda7b45ea5bf7c7c81cf17ee51ed47188b6c67
Yana Sirina <yanasirina0@yandex.ru>
2022-10-18 14:42:11 +0500

    Добавила сериализаторы для регистрации и авторизации пользователя

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Tag, Item
+from .models import Tag, Item, User
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -32,4 +32,26 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         exclude = ('user', )
+
+
+class RegisterUser(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8)
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError('Пользователь с таким именем уже есть')
+        return value
+
+
+class LoginUser(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = User.objects.filter(username=attrs['username']).first()
+        if not user or not user.check_password(attrs['password']):
+            raise serializers.ValidationError('Неверное имя пользователя или пароль')
+        return attrs
+
 
